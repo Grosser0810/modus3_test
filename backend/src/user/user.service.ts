0744fa3ae0602db1b24c5e56fe3b0c6c0c759c21
Hleb Bulanau <g.bulanov@softteco.com>
2022-12-22 14:05:48 +0300

    init frontend

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -18,7 +18,7 @@ export class UserService {
     }
 
     async signPayload(payload: Payload) {
-        return sign(payload, process.env.SECRET_KEY || "SECRET_KEY", { expiresIn: '1d' });
+        return sign(payload, process.env.SECRET_KEY, { expiresIn: '1d' });
     }
 
     async create(RegisterDTO: RegisterDTO) {
